cf8bef2018184c6b757d99eb648ce2bf9085c669
Jordan Santell <jordan@jsantell.com>
2017-01-12 15:27:13 -0800

    Add some error handling to Connection model requests, and save some nice queries for later

--- app/models/connection.js
+++ app/models/connection.js
@@ -3,8 +3,8 @@ import Schema from './schema';
 
 const FETCH_OPTIONS = {
   headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
+    'Accept': 'application/edn',
+    'Content-Type': 'application/edn',
   },
 };
 
@@ -13,13 +13,36 @@ const Connection = Immutable.Record({
   schema: new Schema(),
 
   async fetchSchema() {
-    return this.query('{ :schema }');
+    return this.query(`
+      [:find ?e ?i ?type ?cardinality :where
+        [_ :db.install/attribute ?e]
+        [?e :db/ident ?i]
+        [?e :db/valueType ?t]
+        [?t :db/ident ?type]
+        [?e :db/cardinality ?c]
+        [?c :db/ident ?cardinality]]
+    `);
   },
 
   async query(q) {
     const url = `${this.get('address')}/api/query?q=${encodeURIComponent(q)}`;
     const res = await fetch(url, FETCH_OPTIONS);
-    return res.json();
+    if (res.status !== 200) {
+      throw new Error(res.statusText);
+    }
+
+    try {
+      const text = await res.text();
+      return JSON.parse(text);
+    } catch (e) {
+      throw new Error('Could not parse response from server.');
+    }
+  },
+
+  fetchEntity(eid) {
+    return this.query(`
+      [:find ?attrVal ?val :where [${eid} ?attr ?b] [?attr :db/ident ?attrVal]]
+    `)
   },
 
 }, 'Connection');
